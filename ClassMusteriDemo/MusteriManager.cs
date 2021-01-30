57084ee8401ca12973837921500a26d01353b25d
munammetemin.29@gmail.com <munammetemin.29@gmail.com>
2021-01-30 19:26:06 +0300

    Proje dosyası ekle.

---
+++ ClassMusteriDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.Name + " " + musteri.Surname + " " + musteri.Id);
+        }
+
+
+        public void Erase(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Name + " " + musteri.Surname);
+        }
+    
+        public void List(Musteri[] musteriler) 
+        {
+            Console.WriteLine("Müşteiriler : ");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine( musteri.Name + " " + musteri.Surname );
+            }
+        }
+    }
+}
