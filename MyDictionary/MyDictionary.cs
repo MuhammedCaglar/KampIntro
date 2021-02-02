fa568de82bb8dce6a35d60ad29616541b0b5f51e
munammetemin.29@gmail.com <munammetemin.29@gmail.com>
2021-02-02 20:53:02 +0300

    2 Şubat

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+       
+        K[] keys;
+        
+        V[] values;
+       
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+       
+        public void Add(K key , V value)
+        {
+            K[] tempKey = keys;
+            keys = new K[keys.Length+1];
+            
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+            
+            keys[keys.Length - 1] = key;
+
+            V[] tempValue = values;
+            values = new V[values.Length + 1];
+           
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                values[j] = tempValue[j];
+            }
+           
+           
+            values[values.Length - 1] = value;
+
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(123,"Mehmet");
+
+
+        }
+    }
+}
